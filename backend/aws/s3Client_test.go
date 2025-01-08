95362f65b19e1a0560f262f87ef8b8bdb72ec65c
ireuven89 <aitzik89@gmail.com>
2025-01-08 10:09:11 +0200

    added aws tests

--- backend/aws/s3Client_test.go
+++ backend/aws/s3Client_test.go
@@ -30,64 +30,127 @@ func (mc *MockClient) GetObject(key, bucket string) (interface{}, error) {
 	return args.Get(0).(interface{}), args.Error(1)
 }
 
-func TestClient_DeleteObject(t *testing.T) {
-	client := &MockClient{mock: mock.Mock{}}
-
-	client.mock.On("DeleteObject", "mock-key", "mock-bucket").Return(nil)
-
-	err := client.DeleteObject("mock-key", "mock-bucket")
-
-	assert.Nil(t, err)
+type deleteObjectTest struct {
+	name    string
+	wantErr bool
+	input   struct {
+		key    string
+		bucket string
+	}
+	mockCall *mock.Call
 }
 
-func TestClient_DeleteObjectFail(t *testing.T) {
+func TestClient_DeleteObject(t *testing.T) {
 	client := &MockClient{mock: mock.Mock{}}
+	tests := []deleteObjectTest{
+		{
+			name:    "success",
+			wantErr: false,
+			input: struct {
+				key    string
+				bucket string
+			}{key: "mock-key", bucket: "mock-bucket"},
+			mockCall: client.mock.On("DeleteObject", "mock-key", "mock-bucket").Return(nil),
+		},
+		{
+			name:    "fail invalid key",
+			wantErr: true,
+			input: struct {
+				key    string
+				bucket string
+			}{key: "", bucket: "mock-bucket"},
+			mockCall: client.mock.On("DeleteObject", "", "mock-bucket").Return(errors.New("invalid key for s3")),
+		},
+	}
 
-	client.mock.On("DeleteObject", "mock-key", "").Return(errors.New("error deleting object - bucket not exists"))
+	for _, test := range tests {
+		err := client.DeleteObject(test.input.key, test.input.bucket)
+		assert.Equal(t, err != nil, test.wantErr)
+	}
+}
 
-	err := client.DeleteObject("mock-key", "")
-
-	assert.NotNil(t, err)
+type getObjectTest struct {
+	name    string
+	wantErr bool
+	input   struct {
+		key    string
+		bucket string
+	}
+	mockCall *mock.Call
+	expected interface{}
 }
 
 func TestClient_GetObject(t *testing.T) {
 	client := &MockClient{mock: mock.Mock{}}
-	expectedResult := []byte{'t', 'h', 'e'}
+	tests := []getObjectTest{
+		{
+			name:    "success",
+			wantErr: false,
+			input: struct {
+				key    string
+				bucket string
+			}{key: "mock-key", bucket: "mock-bucket"},
+			mockCall: client.mock.On("GetObject", "mock-key", "mock-bucket").Return([]byte{'r', 'e', 's'}, nil),
+			expected: []byte{'r', 'e', 's'},
+		},
+		{
+			name:    "fail invalid key",
+			wantErr: true,
+			input: struct {
+				key    string
+				bucket string
+			}{key: "", bucket: "mock-bucket"},
+			mockCall: client.mock.On("GetObject", "", "mock-bucket").Return([]byte{}, errors.New("invalid key for s3")),
+			expected: []byte{},
+		},
+	}
 
-	client.mock.On("GetObject", "mock-key", "mock-bucket").Return([]byte{'t', 'h', 'e'}, nil)
-
-	res, err := client.GetObject("mock-key", "mock-bucket")
-
-	assert.Nil(t, err)
-	assert.NotEmpty(t, res)
-	assert.Equal(t, expectedResult, res)
+	for _, test := range tests {
+		res, err := client.GetObject(test.input.key, test.input.bucket)
+		assert.Equal(t, err != nil, test.wantErr)
+		assert.Equal(t, res, test.expected)
+	}
 }
 
-func TestClient_GetObjectFail(t *testing.T) {
-	client := &MockClient{mock: mock.Mock{}}
-
-	client.mock.On("GetObject", "mock-key", "").Return([]byte{}, errors.New("bucket not found error"))
-
-	res, err := client.GetObject("mock-key", "")
-
-	assert.NotNil(t, err)
-	assert.Empty(t, res)
-}
-
-func TestClient_PutObjectFail(t *testing.T) {
-	client := &MockClient{mock: mock.Mock{}}
-	file, _ := os.Open("failed_file.exe")
-	client.mock.On("PutObject", "mock-key", "mock-bucket", file).Return(errors.New("file not exits"))
-
-	err := client.PutObject("mock-key", "mock-bucket", file)
-	assert.NotNil(t, err)
+type putObjectTest struct {
+	name    string
+	wantErr bool
+	input   struct {
+		key    string
+		bucket string
+		file   *os.File
+	}
+	mockCall *mock.Call
+	expected interface{}
 }
 
 func TestClient_PutObject(t *testing.T) {
 	client := &MockClient{mock: mock.Mock{}}
-	file, _ := os.Open("s3client.go")
-	client.mock.On("PutObject", "mock-key", "mock-bucket", file).Return(nil)
+	tests := []putObjectTest{
+		{
+			name:    "success",
+			wantErr: false,
+			input: struct {
+				key    string
+				bucket string
+				file   *os.File
+			}{key: "mock-key", bucket: "mock-bucket"},
+			mockCall: client.mock.On("PutObject", "mock-key", "mock-bucket", &os.File{}).Return(nil),
+		},
+		{
+			name:    "fail invalid key",
+			wantErr: true,
+			input: struct {
+				key    string
+				bucket string
+				file   *os.File
+			}{key: "", bucket: "mock-bucket"},
+			mockCall: client.mock.On("PutObject", "", "mock-bucket", &os.File{}).Return(errors.New("invalid key for s3")),
+		},
+	}
 
-	err := client.PutObject("mock-key", "mock-bucket", file)
-	assert.Nil(t, err)
+	for _, test := range tests {
+		err := client.PutObject(test.input.key, test.input.bucket, &os.File{})
+		assert.Equal(t, err != nil, test.wantErr)
+	}
 }
